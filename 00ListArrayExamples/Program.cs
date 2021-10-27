d7950e6583e5a5ff8302b67af30f7010a4580376
burakyuz1 <burakyuzz@gmail.com>
2021-10-27 12:25:28 +0300

    27/10/2021 Uygulama Dersi

--- 00ListArrayExamples/Program.cs
+++ 00ListArrayExamples/Program.cs
@@ -17,15 +17,56 @@ namespace _00ListArrayExamples
             Random rnd = new Random();
             for (int i = 0; i < 6; i++)
                 sayilar.Add(rnd.Next(0, 10));
-            
-            Console.WriteLine(string.Join(", ",sayilar));
-            Console.WriteLine("Eleman sayısı: {0}",sayilar.Count);
+            Console.WriteLine(string.Join(", ", sayilar));
+
+            //Listenin elemanlarını küçükten büyüğe sıralayın. Ekrana yazdırnıız.
+            Console.WriteLine("\nKüçükten büyüğe sıralanmış hali: \n***********");
+            sayilar.Sort();
+            Console.WriteLine(string.Join(", ", sayilar));
+            Console.WriteLine("Eleman sayısı: {0}", sayilar.Count);
+
+            //Listenin elemanlarını büyükten küçüğe sıralayıp ekrana yazdırın
+            Console.WriteLine("\nBüyükten küçüğe sıralanmış hali\n***********");
+            sayilar.Reverse();
+            //sayilar.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine(string.Join(", ", sayilar));
+
+            // İçerisinde Ankara İstanbul İzmir Bursa Malatya Niğde Kocaeli şehirlerini barındıran bir liste tanımlayınız ve kullanıcıdan bir aranacak şehir isteyiniz eğer listede bu şehir varsa aradığınız şehir listede bulunmaktadır. ylistede yoksa aradığınız şehir listede yoktur. Contains Metot ile
 
 
+            List<string> sehirler = new List<string>() { "ankara", "istanbul", "izmir", "bursa", "malatya", "niğde", "kocaeli" };
+            sehirler.Add("izmir");
+            Console.WriteLine("Aranacak şehir giriniz : (örn. Ankara)");
+            string aranacakSehir = Console.ReadLine().ToLower();
+            string sonuc = sehirler.Contains(aranacakSehir) ? "Aradığın şehir listede var." : "Aradığın şehir listede yok ";
+            Console.WriteLine(sonuc);
 
+            //Sort metodu string/charları da sıralayabilir.
 
+            sehirler.Sort();
+            //Alfabetik sıralı şehirler listesinde İzmir hangi indexte yer alır 
+            Console.WriteLine(sehirler.IndexOf("izmir"));
+            //LastIndexOf metoduyla sondan indexi alır.
 
+            //5 elemanlı bir sayı listesinin 4 elemanının min ve max toplamını hesaplayınız
 
+            List<int> sayiListesi = new List<int>() { 555, 100, 200, 333, 444 };
+            int maxToplam = 0, minToplam = 0;
+            int max = sayiListesi.Max();
+            int min = sayiListesi.Min();
+            for (int i = 0; i < sayiListesi.Count; i++)
+            {
+                if (sayiListesi[i] != max)
+                    minToplam += sayiListesi[i];
+                else
+                    max = int.MaxValue;
+
+                if (sayiListesi[i] != min)
+                    maxToplam += sayiListesi[i];
+                else
+                    min = 0;
+            }
+            Console.WriteLine("Minimum toplam {0} , Maximum toplam {1}", minToplam, maxToplam);
             Console.ReadKey();
 
         }
---
+++ 01MetotExcercises/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01MetotExcercises
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //4 adet metot vardır
+            //***************************************
+            //1. Parametre alan geriye değer döndüren
+            //2. Parametre alan geriye değer döndürmeyen
+            //3. Parametre almayan geriye değer döndüren
+            //4. Parametre almayan geriye değer döndürmeyen
+
+            //2 adet int parametre alan değer döndürmeyen Toplam metodunu yazın.
+            Console.Write("1. sayıyı girin : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2. sayıyı girin : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Toplam(a, b);
+
+
+
+            Console.Write("1. sayıyı girin : ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("2. sayıyı girin : ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("3. sayıyı girin : ");
+            double sayi3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Sayıların çarpımı: "+Carp(sayi1, sayi2, sayi3));
+
+            //Başlangıc ve bitiş değerleri girilen sayı aralığını ekrana yazdıran metodu yazın.
+
+            Console.WriteLine("Başlangıç sayısı girin");
+            int baslangic = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitiş sayısı girin");
+            int bitis = Convert.ToInt32(Console.ReadLine());
+
+            SayiAralik(baslangic, bitis);
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// Verilen 3 sayıyı çarpan metot
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static double Carp(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+        /// <summary>
+        /// Verilen 2 sayıyı toplayan metot
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Toplam(int a, int b)
+        {
+            int toplam = a + b;
+            Console.WriteLine("Girdiğin ilk sayı: {0}, ikinci sayı: {1}, sayıların toplamı: {2}", a, b, toplam);
+        }
+        /// <summary>
+        /// Verilen 2 sayının aralığındaki tüm sayıları ekrana yazdıran metot
+        /// </summary>
+        /// <param name="baslangic"> int degerinde baslangic degeri gir</param>
+        /// <param name="bitis">int degerinde bitis degeri gir</param>
+        static void SayiAralik(int baslangic , int bitis)
+        {
+            for (int i = baslangic ; i <= bitis; i++)
+                Console.Write(i + " ");
+        }
+    }
+}
