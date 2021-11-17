f43e20ef8edd490e0bca0b9d01af549abded5552
Hasret duygu Keskin <hasret_1989_@hotmail.com>
2021-11-17 11:05:58 +0300

    deneme

---
+++ 00RunTimeControls/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _00RunTimeControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnButonUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PCB URET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnButonUret
+            // 
+            this.btnButonUret.Location = new System.Drawing.Point(12, 386);
+            this.btnButonUret.Name = "btnButonUret";
+            this.btnButonUret.Size = new System.Drawing.Size(75, 23);
+            this.btnButonUret.TabIndex = 1;
+            this.btnButonUret.Text = "BTN URET";
+            this.btnButonUret.UseVisualStyleBackColor = true;
+            this.btnButonUret.Click += new System.EventHandler(this.btnButonUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 450);
+            this.Controls.Add(this.btnButonUret);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnButonUret;
+    }
+}
+
---
+++ 00RunTimeControls/Form1.cs
@@ -0,0 +1,87 @@
+﻿using _00RunTimeControls.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _00RunTimeControls
+{
+    public partial class Form1 : Form
+    {
+        Random rnnd = new Random();
+        int siraNo;
+        Label lbl = new Label();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lbl.AutoSize = true;
+            lbl.Font = new Font(FontFamily.GenericSansSerif, 16F);
+            lbl.BackColor = Color.Black;
+            lbl.ForeColor = Color.White;
+            lbl.BorderStyle = BorderStyle.Fixed3D;
+            lbl.Left = ClientSize.Width - lbl.Width + 40;
+            lbl.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+            Controls.Add(lbl);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PictureBox pcb = new PictureBox();
+            pcb.Image = Resources._63197899;
+            pcb.Left = rnnd.Next(0, ClientSize.Width - lbl.Width - pcb.Width);
+            pcb.Top = rnnd.Next(0, ClientSize.Height - lbl.Height - pcb.Height);
+            pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcb.Width = 200;
+            pcb.Height = 200;
+            this.Controls.Add(pcb);
+            this.Icon = Icon.FromHandle(Resources._63197899.GetHicon());
+            pcb.Click += Pcb_Click;
+            lbl.Text = ButtonVePictureAdetGetir().ToString();
+        }
+
+        private void Pcb_Click(object sender, EventArgs e)
+        {
+            PictureBox pcb = (PictureBox)sender;
+            Controls.Remove(pcb);
+            lbl.Text = ButtonVePictureAdetGetir().ToString();
+        }
+
+        private void btnButonUret_Click(object sender, EventArgs e)
+        {
+            siraNo++;
+            Button btn = new Button();
+            btn.Size = new Size(45, 45);
+            btn.Left = rnnd.Next(0, ClientSize.Width - btn.Width);
+            btn.Top = rnnd.Next(0, ClientSize.Height - btn.Height);
+            btn.Text = siraNo.ToString();
+            Controls.Add(btn);
+            btn.Click += Btn_Click;
+            lbl.Text = ButtonVePictureAdetGetir().ToString();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button tiklananButton = (Button)sender;
+            tiklananButton.SendToBack();
+            MessageBox.Show(tiklananButton.Text);
+        }
+
+        private int ButtonVePictureAdetGetir()
+        {
+            int sayac = 0;
+            foreach (var item in this.Controls)
+                if (item is Button || item is PictureBox)
+                    sayac++;
+            return sayac;
+
+
+        }
+    }
+}
